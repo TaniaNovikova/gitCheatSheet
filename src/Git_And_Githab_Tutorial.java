bd1357fe0b717420ff9bbcf92580fe99d59beea2
Novikova Tatiana <tania.novikov@web.de>
2023-11-24 11:06:25 +0100

    tutorial - schmutorial

---
+++ src/Git_And_Githab_Tutorial.java
@@ -0,0 +1,18 @@
+public class Git_And_Githab_Tutorial {
+    /*чтобы задать имя и эл. адрес, используем команду git config
+    * git config --global user.name "John Doe"
+    * git config --global user.email johndoe@example.com
+    *
+    *
+    * запрашиваем список настроек
+    * git config --list
+    *
+    *ls показать папки
+    *
+    * cd открыть папку
+    *
+    * touch .gitignore
+    *
+    *
+    * */
+}
